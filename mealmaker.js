c1b183dc92b30030e0cf0684c04a476cc437d0c7
Ecaterina Pihurov <ecaterinaoprea92@gmail.com>
2021-04-19 19:33:20 +0300

    Third commit

--- mealmaker.js
+++ mealmaker.js
@@ -1,27 +1,27 @@
 const menu = {
     _courses: {
-        _appetizers: [],
-        _mains: [],
-        _desserts: [],
+        appetizers: [],
+        mains: [],
+        desserts: [],
 },
         get appetizers() {
             return this._courses.appetizers;
         },
-        set appetizers(appetizer) {
-            this._courses.appetizers = appetizer;
+        set appetizers(appetizers) {
+            this._courses.appetizers = appetizers;
         },
         get mains() {
             return this._courses.mains;
         },
-        set mains(main) {
-            this._courses.mains = main;
+        set mains(mains) {
+            this._courses.mains = mains;
         },
         get desserts() {
             return this._courses.desserts;
         },
     
-        set desserts(dessert) {
-            this._courses.desserts = dessert;
+        set desserts(desserts) {
+            this._courses.desserts = desserts;
         },
         
     get courses() {
@@ -34,24 +34,24 @@ const menu = {
 
         addDishToCourse(courseName, dishName, dishPrice) {
             const dish = {
-                dish: dishName,
-                price: dishPrice
+                name: dishName,
+                price: dishPrice,
             }
-            this._courses[courseName] = dish;
+            return this._courses[courseName].push(dish);
         },
 
         getRandomDishFromCourse(courseName) {
-            const dishes = this[courseName];
+            const dishes = this._courses[courseName];
             const randomIndex = Math.floor(Math.random() * dishes.length);
             return dishes[randomIndex];
         },
 
         generateRandomMeal() {
-            let appetizer = this.getRandomDishFromCourse('appetizers');
-            let main = this.getRandomDishFromCourse('mains');
-            let dessert = this.getRandomDishFromCourse('desserts');
-            const totalPrice = this.appetizers.price + this.mains.price + this.desserts.price;
-            return `Your meal is ${this.appetizers}, ${this.mains}, ${this.desserts}. The price is ${totalPrice}.`
+            const appetizer = this.getRandomDishFromCourse('appetizers');
+            const main = this.getRandomDishFromCourse('mains');
+            const dessert = this.getRandomDishFromCourse('desserts');
+            const totalPrice = appetizer.price + main.price + dessert.price;
+            return `Your order is ${appetizer.name}, ${main.name}, ${dessert.name}. The price is ${totalPrice}.`
         },
     };
     menu.addDishToCourse('appetizers', 'onion rings', 13.0);
